9af51a1db292b74648926c9744763ed4261d37db
woosung <jangwooseng97@gmail.com>
2023-06-05 19:35:29 +0900

    fix(hs): 희수님 코드 업데이트

--- app/gpi_test.py
+++ app/gpi_test.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 import time
 from collections import deque
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 servoPin2 = 18
@@ -33,10 +32,167 @@ servo2.start(0)
 servo3 = GPIO.PWM(servoPin3, 50)
 servo3.start(0)
 servo4 = GPIO.PWM(servoPin4, 50)
-servo4.start(0)
-
-def test():
-    print('aaaa')
-
-
-test()
+servo4.start(0)  # 서보 PWM 시작, duty = 0이면 서보는 동작하지 않는다.
+def setServoPos(servo, degree):
+    # 각도는 180도를 넘을 수 없다.
+    if degree > 180:
+        degree = 180
+    duty = SERVO_MIN_DUTY + (degree * (SERVO_MAX_DUTY - SERVO_MIN_DUTY) / 180.0)
+    print("Degree: {} to {}(Duty)".format(degree, duty))
+    servo.ChangeDutyCycle(duty)
+def Distance():
+    while True:
+        stop = 0
+        start = 0
+        GPIO.output(TRIG,True)
+        time.sleep(0.00001)        # 10uS의 펄스 발생을 위한 딜레이
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO)==0:
+            start = time.time()     # Echo핀 상승 시간값 저장
+        while GPIO.input(ECHO)==1:
+            stop = time.time()      # Echo핀 하강 시간값 저장
+        check_time = stop - start
+        distance = check_time * 34300 / 2
+        print("Distance : %.1f cm" % distance)
+        time.sleep(0.1)
+        if distance < 10 :
+            break
+def Step_45():
+    step = 0
+    while step < 230:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Step_90():
+    step = 0
+    while step < 465:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Step_135():
+    step = 0
+    while step < 695:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Step_45_reverse():
+    step = 0
+    while step < 230:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(-1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Step_90_reverse():
+    step = 0
+    while step < 465:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(-1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Step_135_reverse():
+    step = 0
+    while step < 695:
+        GPIO.output(AIN1, sig[0])
+        GPIO.output(BIN1, sig[1])
+        GPIO.output(AIN2, sig[2])
+        GPIO.output(BIN2, sig[3])
+        time.sleep(0.01)
+        sig.rotate(-1)
+        step += 1
+    GPIO.output(AIN1, 0)
+    GPIO.output(BIN1, 0)
+    GPIO.output(AIN2, 0)
+    GPIO.output(BIN2, 0)
+def Case1():
+    setServoPos(servo2, 45)
+    setServoPos(servo3, 0)
+    setServoPos(servo4, 45)
+    sleep(1)
+def Case2():
+    setServoPos(servo2, 0)
+    setServoPos(servo3, 0)
+    setServoPos(servo4, 95)
+    sleep(1)
+def Case3():
+    setServoPos(servo2, 45)
+    setServoPos(servo3, 85)
+    setServoPos(servo4, 0)
+    sleep(1)
+try:
+    while True:
+        choice = int(input(" choice : "))
+        if choice == 0:
+            Distance()
+        elif choice == 1:
+            Step_45()
+        elif choice == 2:
+            Step_90()
+        elif choice == 3:
+            Step_135()
+        elif choice == 4:
+            Step_45_reverse()
+        elif choice == 5:
+            Step_90_reverse()
+        elif choice == 6:
+            Step_135_reverse()
+        elif choice == 7:
+            Case1()
+            servo2.start(0)
+            servo3.start(0)
+            servo4.start(0)
+        elif choice == 8:
+            Case2()
+            servo2.start(0)
+            servo3.start(0)
+            servo4.start(0)
+        elif choice == 9:
+            Case3()
+            servo2.start(0)
+            servo3.start(0)
+            servo4.start(0)
+        else:
+            print("잘못된 입력입니다. 다시 시도하세요.")
+    servo2.stop()
+    servo3.stop()
+    servo4.stop()
+except KeyboardInterrupt:
+    pass
+GPIO.cleanup()
